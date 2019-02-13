5a0a5c4e3dd39a743af3bf6169dd718591545118
Lauren Reilly <LaurenReillyNet@gmail.com>
2019-02-12 20:52:27 -0500

    completed name-tags exercise

--- nameTags/App.js
+++ nameTags/App.js
@@ -2,10 +2,23 @@
 const App = (props) => {
     let { nameTags } = props;
     return (
-        <span>name tags</span>
+        <span>
+            {nameTags.map((name, i) => {
+                return <NameTag key={i} name={name} />
+            })}
+        </span>
     )
 }
 
 // CHALLENGE: Write a separate NameTag component for use in the App component
 // TIP: Use props to pass the name to each NameTag component
-const NameTag = null;  
+const NameTag = ({name}) => {
+    return  <div style={{border:"2px solid blue", borderRadius:"5px", margin:"2em", width:"300px"}}>
+                <div style={{backgroundColor: "blue"}}>
+                    <h3 style={{color:"white", textAlign:"center"}}>Hello My Name Is...</h3>
+                </div>
+                <div>
+                    <h1 style={{textAlign: "center"}}>{name}</h1>
+                </div>
+            </div>
+};  
