e265ce19db22cde823f94436c88839f2fc709323
Lauren Reilly <LaurenReillyNet@gmail.com>
2019-02-12 20:35:49 -0500

    completed circles exercise

--- circles/App.js
+++ circles/App.js
@@ -2,8 +2,20 @@
 const App = (props) => {
     let { circles } = props;
     return (
-        <span>circles</span>
+        <span>
+            {circles.map((circle, i) => {
+                return <Circle key={i} color={circle.color} radius={circle.radius}/>
+            })}
+        </span>
     )
 }
 
-const Circle = null;  // CHALLENGE: Write a separate Circle component for use in the App component
+const Circle = ({radius, color}) => {
+    let style = {
+        width: (radius * 2),
+        height: (radius * 2),
+        borderRadius: radius,
+        backgroundColor: color,
+    }
+   return<div style={style}></div>
+};  // CHALLENGE: Write a separate Circle component for use in the App component
