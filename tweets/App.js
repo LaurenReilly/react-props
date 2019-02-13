522215ff98cbb6c46a465fcf72e90f43b8b12864
Lauren Reilly <LaurenReillyNet@gmail.com>
2019-02-12 21:15:27 -0500

    working on tweets render

--- tweets/App.js
+++ tweets/App.js
@@ -8,8 +8,25 @@ const App = (props) => {
 
 const Tweet = null;  // CHALLENGE: Write a separate Tweet component for use in the App component
 
-const User = null;  // CHALLENGE: Write a separate User component for use in the Tweet component
+const User = (props) => {
+    return 
+};  // CHALLENGE: Write a separate User component for use in the Tweet component
 
-const Metrics = null;  // CHALLENGE: Write a separate Metrics component for use in the Tweet component (likes, retweets, replies)
+const Metrics = (props) => {
+    return  <div>
+                <div>
+                    <img src="twitterIcons/heart.svg"/>
+                    <p>{props.likes}</p>
+                </div>
+                <div>
+                    <img src="twitterIcons/message-circle.svg"/>
+                    <p>{props.comments}</p>
+                </div>
+                <div>
+                    <img src="twitterIcons/repeat.svg"/>
+                    <p>{props.retweets}</p>
+                </div>
+            </div>
+};  // CHALLENGE: Write a separate Metrics component for use in the Tweet component (likes, retweets, replies)
 
 // BONUS CHALLENGE - Delete your Tweet component code and rewrite the App component so that it uses User and Metrics directly
