b4000458185d442d9b9857d818b932d86779bda6
Lauren Reilly <LaurenReillyNet@gmail.com>
2019-02-12 20:59:10 -0500

    completed poker exercise

--- poker/App.js
+++ poker/App.js
@@ -2,10 +2,16 @@
 const App = (props) => {
     let { cards } = props;
     return (
-        <span>cards</span>
+        <span>
+            {cards.map((card, i) => {
+                return <Card value={card.value} suit={card.suit}/>
+            })}
+        </span>
     )
 }
 
 // CHALLENGE: Write a separate Card component for use in the App component
 // TIP: Use props to pass the suit and number to each Card component
-const Card = null;  
+const Card = ({value, suit}) => {
+    return <img style={{width:"200px", height:"auto"}} src={"cards/"+ value + suit +".png"}/>
+};  
