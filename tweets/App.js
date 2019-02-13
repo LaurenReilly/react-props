5aece90ccd2d5aaf5d0acab87cd4a9c82976dde9
Lauren Reilly <LaurenReillyNet@gmail.com>
2019-02-13 13:24:33 -0500

    completed tweets exercise

--- tweets/App.js
+++ tweets/App.js
@@ -1,30 +1,54 @@
 // Render the tweets using React!
-const App = (props) => {
-    let { tweets } = props;
-    return (
-        <span>tweets</span>
-    )
+const App = ({tweets}) => {
+    return  <div>
+                {tweets.map((tweet) => {
+                    return <Tweet user={tweet.user} text={tweet.text} likes={tweet.likes} retweets={tweet.retweets} replies={tweet.replies}/>
+                })}
+            </div>
 }
 
-const Tweet = null;  // CHALLENGE: Write a separate Tweet component for use in the App component
 
-const User = (props) => {
-    return 
+const Tweet = ({user, text, likes, retweets, replies}) => {
+    return  <div className="border rounded m-1" style={{width:"500px"}}>
+                <User handle={user.handle} username={user.username} isVerified={user.isVerified} profilePic={user.profilePic}/>
+                <p className="m-5">{text}</p>
+                <Metrics likes={likes} retweets={retweets} replies={replies}/>
+            </div>
+};  // CHALLENGE: Write a separate Tweet component for use in the App component
+
+const User = ({handle, username, isVerified, profilePic}) => {
+    let verified = () => {
+        if (isVerified === true) {
+            return <img src="./twitterIcons/check-circle.svg"/>
+        } else {
+            return null;
+        }
+    }
+    return  <div className="d-flex between" style={{backgroundColor:"#f4f4f4"}}>
+                <img style={{width:"50px", height:"50px"}} src={profilePic}/>
+                {verified()}
+                <h4 className="m-1">{username}</h4>
+                <p className="m-1">{handle}</p>
+            </div>
 };  // CHALLENGE: Write a separate User component for use in the Tweet component
 
-const Metrics = (props) => {
-    return  <div>
-                <div>
+//{() => isVerified ? <img src="twitterIcons/check-circle.svg"/> : null}
+
+//<img src={() => isVerified ? "twitterIcons/check-circle.svg" : "" } />
+
+const Metrics = ({likes, retweets, replies}) => {
+    return  <div className="d-flex between" style={{backgroundColor: "#f4f4f4"}}>
+                <div className="m-2">
                     <img src="twitterIcons/heart.svg"/>
-                    <p>{props.likes}</p>
+                    <p style={{display:"inline-block"}}>{likes}</p>
                 </div>
-                <div>
+                <div className="m-2">
                     <img src="twitterIcons/message-circle.svg"/>
-                    <p>{props.comments}</p>
+                    <p style={{display:"inline-block"}}>{replies}</p>
                 </div>
-                <div>
+                <div className="m-2">
                     <img src="twitterIcons/repeat.svg"/>
-                    <p>{props.retweets}</p>
+                    <p style={{display:"inline-block"}}>{retweets}</p>
                 </div>
             </div>
 };  // CHALLENGE: Write a separate Metrics component for use in the Tweet component (likes, retweets, replies)
